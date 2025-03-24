59949459eac34bc400e4a39b9ed1add2904eda57
Zhu Yixin <67551555+yixin-zhu@users.noreply.github.com>
2025-03-24 17:52:25 +0100

    top 10 sales

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -40,6 +40,9 @@ public class ShopController {
     @ApiOperation("获取店铺的营业状态")
     public Result<Integer> getShopStatus(){
         Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        if(status == null){
+            status = 0;
+        }
         log.info("获取到店铺的营业状态为：{}",status == 1 ? "营业中" : "打烊中");
         return Result.success(status);
     }
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,13 +2,20 @@ package com.sky.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sky.dto.GoodsSalesDTO;
+import com.sky.entity.OrderDetail;
 import com.sky.entity.Orders;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface OrderMapper extends BaseMapper<Orders> {
 
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
